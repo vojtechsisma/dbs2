4ca40918ad04250741020c9050b5b08523f01bd5
Pavel Adamek <pavel.adamek241@gmail.com>
2025-03-06 11:59:18 +0100

    auth

--- be/src/auth/dto/login.dto.ts
+++ be/src/auth/dto/login.dto.ts
@@ -1,9 +1,9 @@
 import { IsEmail, IsNotEmpty, IsString, MinLength } from 'class-validator';
 
 export class LoginDto {
-  @IsEmail()
+  @IsString()
   @IsNotEmpty()
-  email: string;
+  login: string;
 
   @IsString()
   @IsNotEmpty()
