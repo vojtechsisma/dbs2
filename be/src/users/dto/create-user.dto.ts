9974ff195c49eb08c4d4f0f8edc6752ec5f24a01
Pavel Adamek <pavel.adamek241@gmail.com>
2025-03-06 11:59:40 +0100

    usesrs

--- be/src/users/dto/create-user.dto.ts
+++ be/src/users/dto/create-user.dto.ts
@@ -1,8 +1,16 @@
 import { IsEmail, IsNotEmpty, IsString, MinLength } from 'class-validator';
 
 export class CreateUserDto {
+  @IsNotEmpty()
+  @IsString()
+  name: string;
+  
   @IsEmail()
   email: string;
+  
+  @IsNotEmpty()
+  @IsString()
+  login: string;
 
   @IsNotEmpty()
   @IsString()
--- be/src/users/entities/user.entity.ts
+++ be/src/users/entities/user.entity.ts
@@ -1,25 +1,37 @@
 import { Exclude } from 'class-transformer';
-import { ApiHideProperty } from '@nestjs/swagger';
-
-import { PostEntity } from 'src/posts/entities/post.entity';
+import { ApiHideProperty, ApiProperty } from '@nestjs/swagger';
+import { UserRole } from '@prisma/client';
 
 export class UserEntity {
   constructor(partial: Partial<UserEntity>) {
     Object.assign(this, partial);
   }
 
+  @ApiProperty()
   id: number;
 
+  @ApiProperty()
+  name: string;
+
+  @ApiProperty()
   email: string;
+
+  @ApiProperty({ required: false, nullable: true })
+  phone?: string;
+
+  @ApiProperty({ enum: UserRole })
+  role: UserRole;
+
+  @ApiProperty()
+  login: string;
 
   @Exclude()
   @ApiHideProperty()
   password: string;
 
-  @ApiHideProperty()
-  posts?: PostEntity[];
-
+  @ApiProperty()
   createdAt: Date;
 
+  @ApiProperty()
   updatedAt: Date;
 }
